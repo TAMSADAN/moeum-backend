b7ce2abc007287daef0e89563ab37e15f6b180c7
octo <dlqhdgkr456@gmail.com>
2022-02-16 15:14:49 +0900

    first commit

---
+++ src/main/java/moeum/moeum/MoeumApplication.java
@@ -0,0 +1,13 @@
+package moeum.moeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoeumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoeumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moeum/moeum/MoeumApplicationTests.java
@@ -0,0 +1,13 @@
+package moeum.moeum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoeumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
